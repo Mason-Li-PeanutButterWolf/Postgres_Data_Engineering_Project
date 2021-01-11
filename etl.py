a63b56c8ad2589e337d2abebb6f658126aa98fa8
Mason-Li-PeanutButterWolf <63254730+Mason-Li-PeanutButterWolf@users.noreply.github.com>
2021-01-10 23:05:03 -0500

    Update etl.py

--- etl.py
+++ etl.py
@@ -140,7 +140,7 @@ def main():
     conn = conn = psycopg2.connect(host="localhost",
                                 database="sparkifydb",
                                 user="postgres",
-                                password="Ban3duwe")
+                                password="password")
     # conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
