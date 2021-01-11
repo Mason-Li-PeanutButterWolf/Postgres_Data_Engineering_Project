c5f7f4a2ccb0f89fb139b3aa23aaa98247289b29
Mason-Li-PeanutButterWolf <63254730+Mason-Li-PeanutButterWolf@users.noreply.github.com>
2021-01-10 23:02:09 -0500

    Add files via upload

--- etl.py
+++ etl.py
@@ -6,10 +6,31 @@ from sql_queries import *
 import uuid
 
 def flat_list(list_of_list):
+    """
+    Description: This function flatten a list of list to a one dimentional list.
+    Arguments:
+        list_of_list: a list of list
+    Returns:
+        The flattened one dimentional list
+    """
     return [item for sublist in list_of_list for item in sublist]
 
 
 def process_song_file(cur, filepath):
+
+    """
+    Description: This function first read in a song json file into a panda dataframe,
+    and then extract relavent columns for the songs/artists table into a list of values 
+    before loading them into the respective database.
+
+    Arguments:
+        cur: the cursor object.
+        filepath: song data file path.
+
+    Returns:
+        None
+    """
+
     # open song file
     df = pd.read_json(filepath, lines=True)
 
@@ -23,6 +44,22 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+
+    """
+    Description: This function first read in a log json file into a panda dataframe,
+    and then extract and transform relavent columns for the time/user/playsongs table
+    into a list of values before loading them into the respective database.
+
+    Arguments:
+        cur: the cursor object.
+        filepath: song data file path.
+
+    Returns:
+        None
+    """
+
+
+
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -31,6 +68,7 @@ def process_log_file(cur, filepath):
 
     # convert timestamp column to datetime
     t = pd.to_datetime(df.loc[:, ('ts')],unit = 'ms')
+    df['ts'] = t
     
     # insert time data records
     time_data = (t,t.dt.hour,t.dt.day,t.dt.week,t.dt.month,t.dt.year,t.dt.weekday)
@@ -59,12 +97,27 @@ def process_log_file(cur, filepath):
         else:
             songid, artistid = None, None
 
-        # insert songplay record
+        # insert songplay record 
         songplay_data = (str(uuid.uuid4()),row.ts,row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+
+   """
+    Description: This function is responsible for listing the files in a directory,
+    and then executing the ingest process for each file according to the function
+    that performs the transformation to save it to the database.
+
+    Arguments:
+        cur: the cursor object.
+        conn: connection to the database.
+        filepath: log data or song data file path.
+        func: function that transforms the data and inserts it into the database.
+
+    Returns:
+        None
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -84,15 +137,13 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    #change the connection string to the one of your database
     conn = conn = psycopg2.connect(host="localhost",
                                 database="sparkifydb",
                                 user="postgres",
-                                password="password")
+                                password="Ban3duwe")
     # conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
-    
-    #change the file directory to your local directory.
+
     process_data(cur, conn, filepath='D:\data_engineering_project\Data_Modeling_Postgres\data\song_data', func=process_song_file)
     process_data(cur, conn, filepath='D:\data_engineering_project\Data_Modeling_Postgres\data\log_data', func=process_log_file)
 
